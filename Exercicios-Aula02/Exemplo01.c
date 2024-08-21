d0fb6ffdd7774839ab417e5884988f8cf8b63127
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-21 20:03:04 -0300

    Create Exemple 01

---
+++ Exercicios-Aula02/Exemplo01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int numero1 = 3;
+    printf("Numero1 = P=%p C=%i\n", &numero1, numero1);
+    int *numero2 = NULL;
+    printf("Numero2 = P=%p C=\n", numero2);
+    numero2 = &numero1;
+    printf("Numero2 = P=%p C=%i\n", numero2, *numero2);
+
+    return 0;
+}
