476951281be6d21c0e1d184de58b2f502355a5d6
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-21 21:10:35 -0300

    Done Exercise 01

--- Exercicios-Aula02/Exercicio01.c
+++ Exercicios-Aula02/Exercicio01.c
@@ -1,4 +1,25 @@
 /*
 1. Construa um programa para calcular uma equação de segundo grau,
- onde x' e x'' deverão ser um ponteiro, imprima o valor de x'e x'' após a execução do programa.  
+ onde x' e x'' deverão ser um ponteiro, imprima o valor de x'e x'' após a execução do programa.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+ 
+void equacao(float *x1, float *x2, float a, float b, float c);
+ 
+int main(void){
+    
+    float X1, X2;
+    equacao(&X1,&X2,2,3,-4);
+    printf("X1 = %f, X2 = %f",X1,X2);
+ 
+    return 0;
+}
+
+void equacao(float *x1, float *x2, float a, float b, float c){
+    float delta = b * b - 4 * c * a;
+        *x1 = ( - b + sqrt(delta)) / 2 * a;
+        *x2 = ( - b - sqrt(delta)) / 2 * a;
+}
