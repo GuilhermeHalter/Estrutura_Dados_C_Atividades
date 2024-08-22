74d1d46df899a190209a6dcef7842190b656731d
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-22 20:53:34 -0300

    Doing Exercise 09 - Lesson 02

--- Exercicios-Aula02/Exercicio09.c
+++ Exercicios-Aula02/Exercicio09.c
@@ -1,4 +1,16 @@
-/*  
+/*
 9. Faça uma função que calcule a média de uma sequência de valores,
-os valores deverão ser passados por meio de um vetor de referência.  
+os valores deverão ser passados por meio de um vetor de referência.
 */
+
+#include <stdio.h>
+#include <stdio.h>
+
+void media(int *vetor[5]);
+
+int main(void) {
+
+    
+ 
+    return 0;
+}
