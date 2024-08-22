cb287f7197c8536a40d5f31fbbc178bcd42f4d79
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-21 21:17:38 -0300

    Done Exercise 02

--- Exercicios-Aula02/Exercicio02.c
+++ Exercicios-Aula02/Exercicio02.c
@@ -2,3 +2,24 @@
 2. Construa um programa para calcular a área e de um círculo,
 a área deve ser do tipo de ponteiro 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+ 
+int main(void){
+    
+    float *Area;
+    float area, pi, raio;
+    
+    pi= 3.1415;
+    raio= 5;
+    
+    area = pi * pow(raio,2);
+    
+    Area = &area;
+    
+    printf("A area do circulo eh %f", *Area);
+ 
+    return 0;
+}
