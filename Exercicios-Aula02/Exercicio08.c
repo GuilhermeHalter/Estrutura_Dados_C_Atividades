d65875d7a63ff0d27a1109078a242c11103aecc6
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-22 20:43:40 -0300

    Done Exercise 08 - Aula 02

--- Exercicios-Aula02/Exercicio08.c
+++ Exercicios-Aula02/Exercicio08.c
@@ -1,4 +1,31 @@
-/*  
+/*
 8. Faça uma função que leia um texto e converta este texto para maiúscula.
 O texto deve ser passado para função por referência.
 */
+
+#include <stdio.h>
+#include <stdio.h>
+#include <ctype.h>
+
+void maiuscula(char *texto);
+
+int main() {
+    
+    char texto[100];
+    
+    printf("Digite um Texto: ");
+    scanf("%[^\n]s", texto);
+    
+    maiuscula(texto);
+    
+    printf("%s", texto);
+ 
+    return 0;
+}
+
+void maiuscula(char *texto){
+    while(*texto){
+        *texto = toupper((unsigned char)*texto);
+        texto++;
+    }
+}
