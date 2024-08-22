d6b8dc5df59efd65464dc26ab8dc2096c7d143bb
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-22 19:46:51 -0300

    Done Exercise 04 - Aula 02

--- Exercicios-Aula02/Exercicio04.c
+++ Exercicios-Aula02/Exercicio04.c
@@ -1,4 +1,32 @@
 /*
 4. Faça um programa que peça um número para calcular o fatorial. 
-Deve tem uma função para calcular o fatorial (por referência). 
+Deve tem uma função para calcular o fatorial (por referência).
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void fatorial(int *n);
+
+int main()
+{
+    int numero;
+    
+    printf("Digite um numero:");
+    scanf("%i",&numero);
+    
+    fatorial(&numero);
+    
+    return 0;
+}
+
+void fatorial(int *n){
+    int fat;
+    
+    for(fat = 1; *n > 1; *n = *n - 1){
+      fat = fat * *n;
+    }
+
+  printf("\n%d", fat);
+
+}
