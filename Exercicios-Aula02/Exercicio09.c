c1b29d9ce83d05af752c0f03b32477cc553befa3
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-08-28 19:45:17 -0300

    Done Exercise 09 - Aula 02

--- Exercicios-Aula02/Exercicio09.c
+++ Exercicios-Aula02/Exercicio09.c
@@ -6,11 +6,39 @@ os valores deverão ser passados por meio de um vetor de referência.
 #include <stdio.h>
 #include <stdio.h>
 
-void media(int *vetor[5]);
+void media(int *vetor[3], int tamanho);
 
 int main(void) {
-
     
+    int valor1, valor2, valor3;  
+    int *vetor[3];  
+
+    vetor[0] = &valor1;
+    vetor[1] = &valor2;
+    vetor[2] = &valor3;
+    
+    for (int i = 0; i < 3; i++) {
+        printf("Digite o %iº numero: ", i + 1);
+        scanf("%i", vetor[i]);
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        printf("Valor no vetor[%d] = %d\n", i, *vetor[i]);
+    }
+    
+    media(vetor, 3);
  
     return 0;
+}
+
+void media(int *vetor[], int tamanho) {
+    float mediaValores = 0;
+
+    for (int i = 0; i < tamanho; i++) {
+        mediaValores += *vetor[i];
+    }
+
+    mediaValores /= tamanho;
+
+    printf("Média: %.2f\n", mediaValores);
 }
